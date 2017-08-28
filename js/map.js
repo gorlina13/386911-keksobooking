b81683ad820f465fa15cb28d5f7f50a1cd00ef95
Olga Bazhenova <olgabazhenova07@yandex.ru>
2017-08-28 18:21:12 +0300

    Внесла исправления в соответствии с замечаниями

--- js/map.js
+++ js/map.js
@@ -1,237 +1,232 @@
 'use strict';
 
-var OFFER_TITLES = [
-  'Большая уютная квартира',
-  'Маленькая неуютная квартира',
-  'Огромный прекрасный дворец',
-  'Маленький ужасный дворец',
-  'Красивый гостевой домик',
-  'Некрасивый негостеприимный домик',
-  'Уютное бунгало далеко от моря',
-  'Неуютное бунгало по колено в воде'
-];
+(function () {
+  var ESC_KEYCODE = 27;
+  var ENTER_KEYCODE = 13;
 
-var OFFER_TYPES = [
-  'flat',
-  'house',
-  'bungalo'
-];
+  function getRandomNumber(min, max) {
+    var rand = min + Math.random() * (max + 1 - min);
+    rand = Math.floor(rand);
+    return rand;
+  }
 
-var CHECKINS_OR_CHECKOUTS = [
-  '12:00',
-  '13:00',
-  '14:00'
-];
+  function getPlaceTitle(index) {
+    // список заголовков
+    var placeTitles = [
+      'Большая уютная квартира',
+      'Маленькая неуютная квартира',
+      'Огромный прекрасный дворец',
+      'Маленький ужасный дворец',
+      'Красивый гостевой домик',
+      'Некрасивый негостеприимный домик',
+      'Уютное бунгало далеко от моря',
+      'Неуютное бунгало по колено в воде'
+    ];
+    return placeTitles[index];
+  }
 
-var FEATURES = [
-  'wifi',
-  'dishwasher',
-  'parking',
-  'washer',
-  'elevator',
-  'conditioner'
-];
+  function getTypePlace() {
+    var placeTypesEn = ['flat', 'house', 'bungalo'];
+    var index = getRandomNumber(0, 2);
+    return placeTypesEn[index];
+  }
 
-var ESC_KEYCODE = 27;
-var ENTER_KEYCODE = 13;
-var dialogPanels = document.querySelectorAll('.dialog__panel');
-var pinElements = document.querySelectorAll('.pin');
-var activeElement = null;
-var dialog = document.querySelector('.dialog');
-var dialogClose = dialog.querySelector('.dialog__close');
-
-function getRandomInteger(min, max) {
-  var rand = min + Math.random() * (max + 1 - min);
-  rand = Math.floor(rand);
-  return rand;
-}
-
-// Эта функция создает массив объектов с информацией об объявлениях.
-function generateAdverts(advertsCount) {
-
-  var adverts = [];
-
-  for (var i = 0; i < advertsCount; i++) {
-    var coordinateX = getRandomInteger(300, 900);
-    var coordinateY = getRandomInteger(100, 500);
-    adverts[i] = {
-      author: {
-        avatar: 'img/avatars/user0' + (i + 1) + '.png'
-      },
-      offer: {
-        title: OFFER_TITLES[i],
-        address: coordinateX + ', ' + coordinateY,
-        price: getRandomInteger(1000, 1000000),
-        type: OFFER_TYPES[getRandomInteger(0, OFFER_TYPES.length - 1)],
-        rooms: getRandomInteger(1, 5),
-        guests: getRandomInteger(1, 10),
-        checkin: CHECKINS_OR_CHECKOUTS[getRandomInteger(0, CHECKINS_OR_CHECKOUTS.length - 1)],
-        checkout: CHECKINS_OR_CHECKOUTS[getRandomInteger(0, CHECKINS_OR_CHECKOUTS.length - 1)],
-        features: FEATURES.slice(getRandomInteger(0, FEATURES.length - 1)),
-        description: '',
-        photos: []
-      },
-      location: {
-        x: coordinateX,
-        y: coordinateY
-      }
+  function translatePlaceType(englishType) {
+    var translate = {
+      flat: 'Квартира',
+      house: 'Дом',
+      bungalo: 'Бунгало'
     };
-  }
-  return adverts;
-}
-
-// Функция создает новые метки - то есть блоки с классом .pin - для объявлений adverts (см. выше )- и добавляет их в блок tokyo__pin-map.
-function appendPins() {
-  var pinMapElement = document.querySelector('.tokyo__pin-map');
-  var fragment = document.createDocumentFragment();
-  var pinWidth = 40;
-  var pinHeight = 40;
-
-  for (var i = 0; i < generateAdverts(8).length; i++) {
-    var newPinElement = document.createElement('div');
-    newPinElement.className = 'pin';
-    newPinElement.setAttribute('style', 'left: ' + (generateAdverts(8)[i].location.x - pinWidth / 2) + 'px; top: ' + (generateAdverts(8)[i].location.y - pinHeight) + 'px');
-    newPinElement.innerHTML = '<img src="' + generateAdverts(8)[i].author.avatar + '" class="rounded" width="' + pinWidth + '" height="' + pinHeight + '">';
-    fragment.appendChild(newPinElement);
-  }
-  pinMapElement.appendChild(fragment);
-}
-
-// Функция создает список доступных удобств для каждой карточки объявления. Вызывается из функции ниже.
-function createFeatures(array, createdElement) {
-  for (var i = 0; i < array.length; i++) {
-    var newFeatureElement = '<span class="feature__image feature__image--' + array[i] + '"></span>';
-    createdElement.querySelector('.lodge__features').insertAdjacentHTML('beforeend', newFeatureElement);
-  }
-}
-
-// Функция на основе шаблона и массива с данными об объявлениях определяет, из чего будут состоять карточки объявлений.
-function createLodgeElement(advert) {
-  var lodgeTemplate = document.querySelector('#lodge-template').content;
-  var lodgeElement = lodgeTemplate.cloneNode(true);
-  lodgeElement.querySelector('.lodge__title').textContent = advert.offer.title;
-  lodgeElement.querySelector('.lodge__address').textContent = advert.offer.address;
-  lodgeElement.querySelector('.lodge__price').textContent = advert.offer.price + String.fromCharCode(8381) + '/ночь';
-
-
-  if (advert.offer.type === 'flat') {
-    lodgeElement.querySelector('.lodge__type').textContent = 'Квартира';
-  } else if (advert.offer.type === 'house') {
-    lodgeElement.querySelector('.lodge__type').textContent = 'Дом';
-  } else {
-    lodgeElement.querySelector('.lodge__type').textContent = 'Бунгало';
+    return translate[englishType];
   }
 
-  lodgeElement.querySelector('.lodge__rooms-and-guests').textContent = 'Для ' + advert.offer.guests + ' гостей в ' + advert.offer.rooms + ' комнатах';
-  lodgeElement.querySelector('.lodge__checkin-time').textContent = 'Заезд после ' + advert.offer.checkin + ', выезд до ' + advert.offer.checkout;
+  // Генерирует массив строковых значений, взятых случайным образом из начального набора
 
-  createFeatures(advert.offer.features, lodgeElement);
+  function getPlaceFeatures() {
+    var result = [];
+    var featuresList = ['wifi', 'dishwasher', 'parking', 'washer',
+      'elevator', 'conditioner'];
 
-  lodgeElement.querySelector('.lodge__description').textContent = advert.offer.description;
+    for (var i = 0; i < featuresList.length; i++) {
+      if (getRandomNumber(0, 1)) {
+        result.push(featuresList[i]);
+      }
+    }
 
-  document.querySelector('.dialog__title').querySelector('img').setAttribute('src', advert.author.avatar);
-
-  return lodgeElement;
-}
-
-/* Думаю, что карточка объявления - это блок .dialog__panel.
-Блок dialogPanels[0] - это карточка объявления, которое было с самого начала. Ей соответствует .pin.pin__main.
-Пинов (включая .pin.pin__main.) теперь на один больше (1 + 8), чем мы создали новых объявлений (объектов в массиве с инфой об объявлении).
-Объявления созданы функцией generateAdverts(advertsCount) (см. выше). Это не блоки в HTML, а просто информация.
-Для каждого пина после главного нужна своя карточка - свой блок .dialog__panel.
-Она может быть создана на основе шаблона функцией createLodgeElement(advert) (см. выше).
-Здесь advert - это generateAdverts(advertsCount)[i].
-Каждому элементу в массиве generateAdverts(advertsCount)[i] соответствует свой блок .dialog__panel (карточка).
-Как соотнести каждый пин после первого (главного) и каждую карточку?
-Массив пинов на один элемент длиннее, чем массив объектов с инфой об объявлении.
-Поэтому я решила, что надо создавать функцией createLodgeElement карточку с информацией того объявления,
-чей индекс на единицу меньше индекса пина.
-Отсюда запись: generateAdverts(8)[pinElements.indexOf(pin) - 1])
-Но я делаю что-то не так.
-Ниже я пытаюсь вызвать эту функцию switchDialog(pin) в функции openDialog(evt) - которую потом передаю в обработчкик событий.
-Но, возможно, запись switchDialog(activeElement) некорректна. Или что-то еще.
-Наверное, можно и короче, и проще - и чтобы работало. Но не знаю как.*/
-
-function switchDialog(pin) {
-  var dialogPanel = dialog.querySelector('.dialog__panel');
-  dialog.removeChild(dialogPanel);
-  if (pin === pinElements[0]) {
-    dialog.appendChild(dialogPanels[0]);
-  } else {
-    dialog.appendChild(createLodgeElement(generateAdverts(8)[pinElements.indexOf(pin) - 1]));
-  }
-}
-
-appendPins();
-
-// Функция назначает закрытие диалога (сокрытие блока .dialog__panel) на клавишу esc
-function onDialogEscPress(evt) {
-  if (evt.keyCode === ESC_KEYCODE) {
-    closeDialog();
-  }
-}
-
-/* Функция показывает блок .dialog__panel.
-Назначает активным элементом новый пин (на котором событие).
-Вызывает функцию switchDialog, чтобы показать актуальную для данного пина карточку объявления.
-(Должна это делать).
-Добавляет обработчик нажатия esc.*/
-function openDialog(evt) {
-  if (activeElement) {
-    activeElement.classList.remove('pin--active');
+    return result;
   }
 
-  activeElement = evt.currentTarget;
-  activeElement.classList.add('pin--active');
-  switchDialog(activeElement);
-  dialog.classList.remove('hidden');
-  document.addEventListener('keydown', onDialogEscPress);
-}
+  // Создание массива объектов, описывающих объявления
 
-/* Функция скрывает блок .dialog__panel.
-Дезактивирует активный элемент.
-Удаляет обработчик нажатия esc.*/
-function closeDialog() {
-  dialog.classList.add('hidden');
-  if (activeElement) {
-    activeElement.classList.remove('pin--active');
+  function generateAdverts(advertsCount) {
+    var adverts = [];
+
+    for (var i = 0; i < advertsCount; i++) {
+      adverts[i] = {
+        author: {
+          avatar: 'img/avatars/user0' + (i + 1) + '.png'
+        },
+        offer: {
+          title: getPlaceTitle(i),
+          price: getRandomNumber(1000, 1000000),
+          type: getTypePlace(),
+          rooms: getRandomNumber(1, 5),
+          guests: getRandomNumber(1, 10),
+          checkin: getRandomNumber(12, 14) + ':00',
+          checkout: getRandomNumber(12, 14) + ':00',
+          features: getPlaceFeatures(),
+          description: '',
+          photos: []
+        },
+        location: {
+          x: getRandomNumber(300, 900),
+          y: getRandomNumber(100, 500)
+        }
+      };
+      adverts[i].offer.address = // адрес квартиры
+        adverts[i].location.x + ', ' + adverts[i].location.y;
+    }
+
+    return adverts;
   }
-  document.removeEventListener('keydown', onDialogEscPress);
-}
 
-/* Функция для передачи в обработчик клика.
-??? Надо ли здесь передавать evt в обоих случаях? У меня было только внутри - в openDialog - но eslint сказал, что evt - не определен.*/
-function onPinClick(evt) {
-  openDialog(evt);
-}
-
-/* Функция для передачи в обработчик нажатия клавиши.
-??? Надо ли здесь передавать evt в обоих случаях? У меня было только внутри - в openDialog - но eslint сказал, что evt - не определен.*/
-function onPinEnterPress(evt) {
-  if (evt.keyCode === ENTER_KEYCODE) {
-    openDialog(evt);
+  function createFeaturesFragment(adverts) {
+    var fragment = document.createDocumentFragment();
+    for (var i = 0; i < adverts.offer.features.length; i++) {
+      var feature = document.createElement('span');
+      feature.classList.add('feature__image');
+      feature.classList.add('feature__image--' + adverts.offer.features[i]);
+      fragment.appendChild(feature);
+    }
+    return fragment;
   }
-}
 
-// Функция для передачи в обработчик клика по крестику.
-function onDialogCloseClick() {
-  closeDialog();
-}
+  function createAdNode(adverts) {
+    // Создание узла и запись данных
+    var advertsNode =
+      document.querySelector('#lodge-template').content.cloneNode(true);
 
-// Функция для передачи в обработчик нажатия enter, когда крестик в фокусе.
-function onDialogCloseEnterPress(evt) {
-  if (evt.keyCode === ENTER_KEYCODE) {
-    closeDialog();
+    advertsNode.querySelector('.lodge__title').textContent =
+      adverts.offer.title;
+
+    advertsNode.querySelector('.lodge__address').textContent =
+      adverts.offer.address;
+
+    advertsNode.querySelector('.lodge__price').innerHTML =
+      adverts.offer.price + '&#x20bd;/ночь';
+
+    advertsNode.querySelector('.lodge__type').textContent =
+      translatePlaceType(adverts.offer.type);
+
+    advertsNode.querySelector('.lodge__rooms-and-guests').textContent =
+      'Для ' + adverts.offer.guests +
+      ' гостей в ' + adverts.offer.rooms + ' комнатах';
+
+    advertsNode.querySelector('.lodge__checkin-time').textContent =
+      'Заезд после ' + adverts.offer.checkin +
+      ', выезд до ' + adverts.offer.checkout;
+
+    var setOfFeatures = createFeaturesFragment(adverts);
+    advertsNode.querySelector('.lodge__features').appendChild(setOfFeatures);
+
+    advertsNode.querySelector('.lodge__description').textContent =
+      adverts.offer.description;
+
+    // Замена стандартной аватарки на аватарку отрисовываемого объявления
+    document.getElementById('offer-dialog').getElementsByTagName('img')[0].src =
+      adverts.author.avatar;
+
+    return advertsNode;
   }
-}
 
-// Добавляю атрибут tabindex (так можно?), вешаю обработчики.
-for (var i = 0; i < pinElements.length; i++) {
-  pinElements[i].querySelector('img').setAttribute('tabindex', '0');
-  pinElements[i].addEventListener('click', onPinClick);
-  pinElements[i].addEventListener('keydown', onPinEnterPress);
-}
+  function createPinsFragment(adverts) {
+    var fragment = document.createDocumentFragment();
 
-dialogClose.setAttribute('tabindex', '0');
-dialogClose.addEventListener('click', onDialogCloseClick);
-dialogClose.addEventListener('keydown', onDialogCloseEnterPress);
+    for (var i = 0; i < adverts.length; i++) {
+      var pin = document.createElement('div');
+      pin.className = 'pin';
+      pin.style.left = adverts[i].location.x + 'px';
+      pin.style.top = adverts[i].location.y + 'px';
+      pin.setAttribute('tabindex', 0);
+      pin.setAttribute('data-ad-number', i);
+      pin.addEventListener('click', dialogOpenHandler);
+      pin.addEventListener('keypress', dialogOpenHandler);
+      pin.adverts = adverts;
+
+      var pinAvatar = document.createElement('img');
+      pinAvatar.src = adverts[i].author.avatar;
+      pinAvatar.className = 'rounded';
+      pinAvatar.width = '40';
+      pinAvatar.height = '40';
+
+      pin.appendChild(pinAvatar);
+      fragment.appendChild(pin);
+    }
+    return fragment;
+  }
+
+  function showPins(setOfAds) {
+    var pinsFragment = createPinsFragment(setOfAds);
+    document.querySelector('.tokyo__pin-map').appendChild(pinsFragment);
+  }
+
+
+  function showAdDialog(adObject) {
+    var adNode = createAdNode(adObject);
+    var dialogPanel = document.querySelector('.dialog__panel');
+    var parentNode = dialogPanel.parentNode;
+    parentNode.replaceChild(adNode, dialogPanel);
+    document.querySelector('.dialog').classList.remove('hidden');
+  }
+
+  function initializeAdvert(count) {
+    var adverts = generateAdverts(count);
+    showPins(adverts);
+    document.querySelector('.dialog').classList.add('hidden');
+    document.querySelector('.dialog__close').addEventListener('click', dialogCloseHandler);
+    document.querySelector('.dialog__close').addEventListener('keydown', dialogCloseHandler);
+    document.querySelector('.tokyo').addEventListener('keydown', dialogCloseHandler);
+  }
+
+  function makePinActive(pin) {
+    pin.classList.add('pin--active');
+  }
+
+  function makePinsInactive(pins) {
+    [].forEach.call(pins, function (pin) {
+      pin.classList.remove('pin--active');
+    });
+  }
+
+  function dialogCanOpen(event) {
+    return event.button === 0 || event.keyCode === ENTER_KEYCODE;
+  }
+
+
+  function dialogCanClose(event) {
+    return event.button === 0 || event.keyCode === ENTER_KEYCODE || event.keyCode === ESC_KEYCODE;
+  }
+
+  function dialogOpenHandler(event) {
+    var pin = event.currentTarget;
+    var setOfAds = pin.adverts;
+    var index = pin.dataset.adNumber;
+    var allPins = pin.parentNode.querySelectorAll('.pin--active');
+
+    if (dialogCanOpen(event)) {
+      makePinsInactive(allPins);
+      makePinActive(pin);
+      showAdDialog(setOfAds[index]);
+    }
+  }
+
+
+  function dialogCloseHandler(event) {
+    if (dialogCanClose(event)) {
+      document.querySelector('.dialog').classList.add('hidden');
+      document.querySelector('.pin--active').classList.remove('pin--active');
+    }
+  }
+
+  initializeAdvert(8);
+})();
