c796d31d380637da47daa0951174678dec4faaa7
Olga Bazhenova <olgabazhenova07@yandex.ru>
2017-09-03 20:36:21 +0300

    Добавила реакцию на перемещение по карте метки заполняемого объявления

--- js/map.js
+++ js/map.js
@@ -5,10 +5,87 @@
     var adverts = window.data.generateAdverts(count);
     window.pin.showPins(adverts);
     document.querySelector('.dialog').classList.add('hidden');
-    document.querySelector('.dialog__close').addEventListener('click', window.pin.dialogCloseHandler);
-    document.querySelector('.dialog__close').addEventListener('keydown', window.pin.dialogCloseHandler);
-    document.addEventListener('keydown', window.pin.dialogCloseHandler);
+    document.querySelector('.dialog__close').addEventListener('click', window.pin.onDialogClose);
+    document.querySelector('.dialog__close').addEventListener('keydown', window.pin.onDialogClose);
+    document.addEventListener('keydown', window.pin.onDialogClose);
   }
 
   initializeAdvert(8);
+
+  var mainPin = document.querySelector('.pin__main');
+  var mainPinImg = mainPin.querySelector('img');
+
+  mainPin.addEventListener('mousedown', function (evt) {
+  evt.preventDefault();
+
+    var startCoords = {
+      x: evt.clientX,
+      y: evt.clientY
+    };
+
+    function onMainPinMouseMove(moveEvt) {
+      moveEvt.preventDefault();
+
+      var shift = {
+        x: startCoords.x - moveEvt.clientX,
+        y: startCoords.y - moveEvt.clientY
+      };
+
+      var currentCoords = {
+        x: mainPin.offsetLeft - shift.x,
+        y: mainPin.offsetTop - shift.y
+      };
+
+      var mapBorders = {
+        xMin: 300,
+        xMax: 900,
+        yMin: 100,
+        yMax: 500
+      }
+
+      var mainPinAreaCoords = {
+        xMin: (mapBorders.xMin - mainPinImg.width / 2),
+        xMax: (mapBorders.xMax - mainPinImg.width / 2),
+        yMin: (mapBorders.yMin - mainPinImg.height),
+        yMax: (mapBorders.yMax - mainPinImg.height)
+      };
+
+      var addressCoords = {};
+
+      startCoords = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      };
+
+      if ((currentCoords.x >=  mainPinAreaCoords.xMin) && (currentCoords.x <=  mainPinAreaCoords.xMax)) {
+        mainPin.style.left = currentCoords.x + 'px';
+        addressCoords.x = currentCoords.x + mainPinImg.width / 2;
+      } else if (currentCoords.x <  mainPinAreaCoords.xMin) {
+        addressCoords.x = mapBorders.xMin;
+      } else {
+        addressCoords.x = mapBorders.xMax;
+      }
+
+      if ((currentCoords.y >= mainPinAreaCoords.yMin) && (currentCoords.y <= mainPinAreaCoords.yMax)) {
+        mainPin.style.top = currentCoords.y + 'px';
+        addressCoords.y = currentCoords.y + mainPinImg.height;
+      } else if (currentCoords.y < mainPinAreaCoords.yMin) {
+        addressCoords.y = mapBorders.yMin;
+      } else {
+        addressCoords.y = mapBorders.yMax;
+      }
+
+      document.querySelector('#address').value = 'x: ' + addressCoords.x + ', y: ' + addressCoords.y;
+    };
+
+    function onMainPinMouseUp(upEvt) {
+      upEvt.preventDefault();
+
+      document.removeEventListener('mousemove', onMainPinMouseMove);
+      document.removeEventListener('mouseup', onMainPinMouseUp);
+    };
+
+    document.addEventListener('mousemove', onMainPinMouseMove);
+    document.addEventListener('mouseup', onMainPinMouseUp);
+  });
 })();
